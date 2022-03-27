95d402bd6d47b3d3dd4f58c800066f95cf21e414
ydts2 <ykpetrolhead08@outlook.com>
2022-03-27 11:27:27 +0530

    mars rover

--- Task-04/mars.py
+++ Task-04/mars.py
@@ -1,20 +1,30 @@
 import requests
 import argparse
 import json
-from pdb import set_trace as stx
 
 parser=argparse.ArgumentParser(description="API for pictures from the rovers")
-parser.add_argument("--api_key",default='tE7XQGpjRshsrizdjGXLqcGimd2m4u301zcbh4Sx', type=str, help="API Key")
-parser.add_argument("--earth_date",default="2015-06-03", type=str, help="Date in YYYY-MM-DD")
-parser.add_argument("--rover_id",default='5', type=int, help="Image id")
+parser.add_argument("api_key",default='Ne5QVsmj5rabNTPZwE6d7MUhE1m9lWG4l0zm07Ci', type=str, help="Your API key")
+parser.add_argument("earth_date",default="2015-06-03", type=str, help="Date in YYYY-MM-DD")
+parser.add_argument("rover_id",default= 5, type=int, help="1: Curiosity 2: Opportunity 3: Spirit")
 args = parser.parse_args()
 
 earth_date = args.earth_date
 api_key = args.api_key
 rover_id = args.rover_id
+rover_name = ""
 
-api_url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={earth_date}&api_key={api_key}"
+if rover_id == 5:
+    rover_name = "curiosity"
+elif rover_id == 6:
+    rover_name = "opportunity"
+elif rover_id == 7:
+   rover_name = "spirit"    
+else:
+    print("invalid id !!")
 
+#ap=Ne5QVsmj5rabNTPZwE6d7MUhE1m9lWG4l0zm07Ci
+api_url='https://api.nasa.gov/mars-photos/api/v1/rovers/{}/photos?earth_date={}&api_key={}'.format(rover_name,earth_date,api_key)
+print(api_url)
 request = requests.get(api_url)
 parsed_requests = json.loads(request.text)
 
@@ -28,10 +38,12 @@ else:
             print("Saving Image")
             photo_url = photo['img_src']
             photo_request = requests.get(photo_url)
-            photo_path = open(f"download_{images_saved+1}.png", "wb")
+            photo_path = open(f"new_{images_saved+1}.png", "wb")
             photo_path.write(photo_request.content)
             photo_path.close()            
             images_saved += 1
 
 if not images_saved:
     print("image not found")
+
+    
