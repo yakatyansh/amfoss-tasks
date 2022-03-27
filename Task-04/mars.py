23f6b0ecffd42fe7014e1535a71a35978b912145
ydts2 <ykpetrolhead08@outlook.com>
2022-03-27 14:08:04 +0530

    Final code

--- Task-04/mars.py
+++ Task-04/mars.py
@@ -1,49 +1,45 @@
-import requests
-import argparse
-import json
+import requests,argparse,json
 
-parser=argparse.ArgumentParser(description="API for pictures from the rovers")
-parser.add_argument("api_key",default='Ne5QVsmj5rabNTPZwE6d7MUhE1m9lWG4l0zm07Ci', type=str, help="Your API key")
-parser.add_argument("earth_date",default="2015-06-03", type=str, help="Date in YYYY-MM-DD")
-parser.add_argument("rover_id",default= 5, type=int, help="1: Curiosity 2: Opportunity 3: Spirit")
-args = parser.parse_args()
+prsr=argparse.ArgumentParser(description="API for pictures from the rovers")
+prsr.add_argument("api_key",default='Ne5QVsmj5rabNTPZwE6d7MUhE1m9lWG4l0zm07Ci', type=str, help="Your API key")
+prsr.add_argument("rover_id",default= 5, type=int, help="1: Curiosity 2: Opportunity 3: Spirit")
+prsr.add_argument("earth_date",default="2015-06-03", type=str, help="Date in YYYY-MM-DD")
+args = prsr.parse_args()
 
-earth_date = args.earth_date
+e_date = args.earth_date
 api_key = args.api_key
-rover_id = args.rover_id
-rover_name = ""
+rov_id = args.rover_id
+rov_name = ""
 
-if rover_id == 5:
-    rover_name = "curiosity"
-elif rover_id == 6:
-    rover_name = "opportunity"
-elif rover_id == 7:
-   rover_name = "spirit"    
+if rov_id == 5:
+    rov_name = "curiosity"
+elif rov_id == 6:
+    rov_name = "opportunity"
+elif rov_id == 7:
+   rov_name = "spirit"    
 else:
-    print("invalid id !!")
+    print("invalid ID !!")
 
 #ap=Ne5QVsmj5rabNTPZwE6d7MUhE1m9lWG4l0zm07Ci
-api_url='https://api.nasa.gov/mars-photos/api/v1/rovers/{}/photos?earth_date={}&api_key={}'.format(rover_name,earth_date,api_key)
+api_url='https://api.nasa.gov/mars-photos/api/v1/rovers/{}/photos?earth_date={}&api_key={}'.format(rov_name,e_date,api_key)
 print(api_url)
 request = requests.get(api_url)
 parsed_requests = json.loads(request.text)
 
-images_saved=0
+img_saved=0
 if 'error' in parsed_requests:
     print(parsed_requests['error'])
 else:
     for photo in parsed_requests['photos']:
-        if rover_id == photo['rover']['id']:
+        if rov_id == photo['rover']['id']:
             save_flag = True
             print("Saving Image")
             photo_url = photo['img_src']
             photo_request = requests.get(photo_url)
-            photo_path = open(f"new_{images_saved+1}.png", "wb")
+            photo_path = open("downloaded_file_{}.png".format(img_saved+1), "wb")
             photo_path.write(photo_request.content)
             photo_path.close()            
-            images_saved += 1
+            img_saved += 1
 
-if not images_saved:
+if not img_saved:
     print("image not found")
-
-    
